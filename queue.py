3ee42ac086b6dad8685ac636c4fe3dd53483638e
Mia Song <mia721015@gmail.com>
2021-12-09 19:41:06 +0800

    deque

--- queue.py
+++ queue.py
@@ -15,6 +15,31 @@ class Queue:
     
     def size(self):
         return len(self.__list)
+
+# Deque 雙端隊列 (double-ended queue)
+class Deque:
+    def __init__(self):
+        self.__list = []
+    
+    def is_empty(self):
+        return self.__list==[]
+    
+    def add_front(self, item):
+        self.__list.insert(0, item)
+    
+    def add_rear(self, item):
+        self.__list.append(item)
+    
+    def pop_front(self):
+        item = self.__list.pop(0)
+        return item
+    
+    def pop_rear(self):
+        item = self.__list.pop(-1)
+        return item
+    
+    def size(self):
+        return len(self.__list)
     
 if __name__=="__main__":
     q = Queue()
